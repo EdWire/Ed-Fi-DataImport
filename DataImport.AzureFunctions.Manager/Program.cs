92d730229464bc0b7a20ffba59f5b580e3f0941c
Ashwin <ashwin027@gmail.com>
2024-03-26 18:00:04 -0600

    Updated packages

--- DataImport.AzureFunctions.Manager/Program.cs
+++ DataImport.AzureFunctions.Manager/Program.cs
@@ -7,23 +7,14 @@ using Microsoft.Extensions.Logging.Console;
 using DataImport.AzureFunctions.Manager.Services;
 using System.Net;
 using DurableFunctionsMonitor.DotNetBackend;
+using Microsoft.ApplicationInsights.WorkerService;
 
 var host = new HostBuilder()
-    .ConfigureFunctionsWorkerDefaults(builder =>
-    {
-        // Is added by package "Microsoft.Azure.Functions.Worker.ApplicationInsights".
-        // Documented here because it is still preview: https://github.com/Azure/azure-functions-dotnet-worker/pull/944#issue-1282987627
-        builder
-            .AddApplicationInsights()
-            .AddApplicationInsightsLogger();
-    })
+    .ConfigureFunctionsWorkerDefaults()
     .ConfigureServices((ctx, serviceProvider) =>
     {
-        // You will need extra configuration because above will only log per default Warning (default AI configuration) and above because of following line:
-        // https://github.com/microsoft/ApplicationInsights-dotnet/blob/main/NETCORE/src/Shared/Extensions/ApplicationInsightsExtensions.cs#L427
-        // This is documented here:
-        // https://github.com/microsoft/ApplicationInsights-dotnet/issues/2610#issuecomment-1316672650
-        // So remove the default logger rule (warning and above). This will result that the default will be Information.
+        serviceProvider.AddApplicationInsightsTelemetryWorkerService();
+        serviceProvider.ConfigureFunctionsApplicationInsights();
         serviceProvider.Configure<LoggerFilterOptions>(options =>
         {
             var toRemove = options.Rules.FirstOrDefault(rule => rule.ProviderName
